d2f7d974b12b7bbbeb640b998d1bdf38a18f39bf
Andrew <andrewvu.cs@gmail.com>
2020-02-20 14:41:15 -0800

    went over commonly useful unused types such as Tuple and Enum

--- typescript.js
+++ typescript.js
@@ -8,11 +8,23 @@ var eyeColor = "brown";
 var favouriteQuote = "I'm not old, i'm only " + age;
 // Array
 var pets = ["cat", "dog", "pig"];
-var pets2 = ['lion', 'dragon', 'lizard'];
+var pets2 = ["lion", "dragon", "lizard"];
 // Object
 var wizard = {
-    a: 'John'
+    a: "John"
 };
 // null and undefined
 var meh = undefined;
 var noo = null;
+// Tuple
+var basket;
+basket = ["basketball", 5];
+// Enum
+var Size;
+(function (Size) {
+    Size[Size["Small"] = 1] = "Small";
+    Size[Size["Medium"] = 2] = "Medium";
+    Size[Size["Large"] = 3] = "Large";
+})(Size || (Size = {}));
+;
+var sizeName = Size.Small;
--- typescript.ts
+++ typescript.ts
@@ -12,14 +12,21 @@ let favouriteQuote: string = `I'm not old, i'm only ${age}`;
 
 // Array
 let pets: string[] = ["cat", "dog", "pig"];
-let pets2: Array<string> = ['lion', 'dragon', 'lizard'];
+let pets2: Array<string> = ["lion", "dragon", "lizard"];
 
 // Object
 let wizard: object = {
-    a: 'John'
-}
+  a: "John"
+};
 
 // null and undefined
 let meh: undefined = undefined;
 let noo: null = null;
 
+// Tuple
+let basket: [string, number];
+basket = ["basketball", 5];
+
+// Enum
+enum Size { Small = 1, Medium = 2, Large = 3};
+let sizeName: number = Size.Small;
